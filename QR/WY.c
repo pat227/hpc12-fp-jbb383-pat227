1c6bcabceac2169a149b3290ac01d70c8977f617
jbb383 <jbb383@nyu.edu>
2012-12-04 13:17:59 -0500

    new file:   WY.c 	modified:   Makefile 	modified:   MatrixMatrixMultiply.c 	modified:   QR.h 	modified:   main.c

---
+++ QR/WY.c
@@ -0,0 +1,22 @@
+/* HPC 2012 Project  : Jacqueline Bush, Paul Torres */
+
+/* WY Code: 
+Given a Matrix A this code outputs the transpose of the orthogonal matrix Q
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <omp.h>
+#include <time.h>
+#include "QR.h"
+
+
+WY( double A*, w, h, double Q*){
+
+ 
+printf("hi!\n");
+
+
+}
+
