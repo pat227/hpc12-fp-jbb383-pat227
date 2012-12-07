778d8ce991f6495e263e152b0ced59690835e706
jbb383 <jbb383@nyu.edu>
2012-12-07 13:53:13 -0500

    new file:   WY.h

---
+++ QR/WY.h
@@ -0,0 +1,7 @@
+/* HPC 2012 Final Project: Jacqueline Bush, Paul Torries */
+
+/* Header file for WY code */
+
+void CalculateV( double *, int, int, int, double *);
+void CalculateZ( double *, double *,double *, int, double *);
+int WY( double *, int , int , double *);
