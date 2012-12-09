3ab5272ec3c9fcc42e547002b19879bd95e8c634
jbb383 <jbb383@nyu.edu>
2012-12-09 16:32:07 -0500

    Currently empty....
    	new file:   BlockedQR.c
    	new file:   BlockedQR.h

---
+++ QR/BlockedQR.c
@@ -0,0 +1,28 @@
+/* HPC 2012 Project  : Jacqueline Bush, Paul Torres */
+
+/* BlockedQR FILE: 
+Performs Blocked QR factorization/
+*/
+
+/* Headers: */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include "WY.h"
+#include "test.h"
+#include "MatrixMatrixMultiply.h"
+#include "MatrixTranspose.h"
+#include "Utilities.h"
+#include "BlockedQR.h"
+
+
+void BlockedQR( double *A, int h, int w, double *Q, double *R){
+
+
+
+
+
+}
+
+
