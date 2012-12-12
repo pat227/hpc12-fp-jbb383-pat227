c920eb7191b15248153f8d192f73a93dec079f16
jbb383 <jbb383@nyu.edu>
2012-12-11 23:29:06 -0500

    BlockedQR works if h and w are multiplies of b.

--- QR/BlockedQR.c
+++ QR/BlockedQR.c
@@ -25,8 +25,7 @@ void BlockedQR( double *A, int h, int w, double *Q, double *R){
 	prettyPrint(A, h, w);
 
 /* Block size */
-	int b= 2 ;
-	printf(" block size = %d \n", b);
+	int b= 3 ;
 
 /* Calculate Number of blocks */
 	int wn_bloc = (w+b-1)/b; // Number of Blocks in width (round up)
@@ -41,6 +40,8 @@ void BlockedQR( double *A, int h, int w, double *Q, double *R){
 	if( wn_bloc > hn_bloc )
 		n = hn_bloc;
 
+	printf("n = %d \n", n);
+
 /* Set Q = m by m identity matrix */
  	for(int i = 0; i<h; i++){
 		for(int j=0; j<h; j++){
@@ -51,12 +52,12 @@ void BlockedQR( double *A, int h, int w, double *Q, double *R){
 	}
 
 /* Initialize Blocks */
-	double *Q1_block_1b_by_1b = malloc( b * b * sizeof( double));
-	double *Q2_block_1b_by_1b = malloc( b* b* sizeof(double));
-	double *Q3_block_1b_by_1b = malloc( b* b* sizeof(double));
-	double *Q1_block_2b_by_2b = malloc( 4*b * b * sizeof( double));
-	double *Q2_block_2b_by_2b = malloc( 4*b* b* sizeof(double));
-	double *Q3_block_2b_by_2b = malloc( 4*b* b* sizeof(double));
+	double *Q_block_1b_by_1b = malloc( b * b * sizeof( double));
+	double *Q1_block_h_by_1b = malloc( h* b* sizeof(double));
+	double *Q2_block_h_by_1b = malloc( h* b* sizeof(double));
+	double *Q_block_2b_by_2b = malloc( 4*b * b * sizeof( double));
+	double *Q1_block_h_by_2b = malloc( 2*h* b* sizeof(double));
+	double *Q2_block_h_by_2b = malloc( 2*h* b* sizeof(double));
 	double *Qt_block_1b_by_1b = malloc( b * b * sizeof( double));
 	double *Qt_block_2b_by_2b = malloc( 2*b * 2 * b * sizeof(double));
 	double *R_block_1b_by_1b = malloc( 2*b* b* sizeof(double));
@@ -65,19 +66,22 @@ void BlockedQR( double *A, int h, int w, double *Q, double *R){
 	double *A_block_2b_by_1b = malloc(2*b * b* sizeof(double));
 
 	double *temp = malloc( h*w*sizeof(double));
+	double *temp1 = malloc(h*h*sizeof(double));
+	double *Qt = malloc(h*h*sizeof(double));
+	double *temp_block = malloc(4*b*b*sizeof(double));
 
 /* Enter Loop */
 for(int k =0; k< n; k++){	
 	/* Update Diagonal Block */	
 	BlockMatrix(A, A_block_1b_by_1b, h, w, b, k, k);
-	WY( A_block_1b_by_1b, b, b, Q2_block_1b_by_1b, Qt_block_1b_by_1b, R_block_1b_by_1b); 
+	WY( A_block_1b_by_1b, b, b, Q_block_1b_by_1b, Qt_block_1b_by_1b, R_block_1b_by_1b); 
 	UnBlockMatrix(A, R_block_1b_by_1b, h, w, b, k,k);
 
 	/* Update Corresponding Diagonal Block in in Q */
-	BlockMatrix(Q, Q1_block_1b_by_1b, h, w, b, k, k);
-	MatrixMatrixMultiply(Q1_block_1b_by_1b, b, b, Q2_block_1b_by_1b, b, b, Q3_block_1b_by_1b);	
-	UnBlockMatrix(Q, Q3_block_1b_by_1b, h, h, b, k,k);		
-
+	BlockQ1(Q, Q1_block_h_by_1b, h, h, b, k );
+	MatrixMatrixMultiply(Q1_block_h_by_1b, h, b, Q_block_1b_by_1b, b, b, Q2_block_h_by_1b);	
+	UnBlockQ1(Q2_block_h_by_1b , Q, h, h, b, k);
+			
 	/* Update Blocks along row with Digonal Block */
 	for( int j=k+1; j<wn_bloc ; j++){
 	BlockMatrix(A, A_block_1b_by_1b, h, w, b, k, j);
@@ -86,15 +90,15 @@ for(int k =0; k< n; k++){
 	}
 
 	/* Update Blocks below the Diagonal (will also update diagonal) */
-	for( int i=k+1; i< hn_bloc; i++){
+	for( int i=k+1; i<hn_bloc ; i++){
 		Block(A_block_2b_by_1b, A, h , w, b, k, i, k);	
-		WY( A_block_2b_by_1b, 2*b, b, Q2_block_2b_by_2b, Qt_block_2b_by_2b, R_block_2b_by_1b);
+		WY( A_block_2b_by_1b, 2*b, b, Q_block_2b_by_2b, Qt_block_2b_by_2b, R_block_2b_by_1b);
 		UnBlock(A, R_block_2b_by_1b, h, w, b,k, i, k);
 
 		/* Update Q */
-		BlockQ(Q, Q1_block_2b_by_2b, h, h, b, k, k, i, i); 		
-		MatrixMatrixMultiply(Q1_block_2b_by_2b, 2*b, 2*b, Q2_block_2b_by_2b, 2*b, 2*b, Q3_block_2b_by_2b);		
-		UnBlockQ( Q3_block_2b_by_2b, Q, h, h, b, k , k, i, i);	
+		BlockQ(Q, Q1_block_h_by_2b, h, h, b, k, i );		
+		MatrixMatrixMultiply(Q1_block_h_by_2b, h, 2*b, Q_block_2b_by_2b, 2*b, 2*b, Q2_block_h_by_2b);		
+		UnBlockQ( Q2_block_h_by_2b, Q, h, h, b, k, i);	
  
 		/* Update Blocks along i and k  rows */
 		for( int j=k+1; j<wn_bloc ; j++){
@@ -102,11 +106,21 @@ for(int k =0; k< n; k++){
 			MatrixMatrixMultiply(Qt_block_2b_by_2b, 2*b, 2*b, A_block_2b_by_1b, 2*b, b, R_block_2b_by_1b);
 			UnBlock(A, R_block_2b_by_1b, h, w, b,k, i, j);	
 		}
+
 	}
 
+	
+}
 
 
-	MatrixMatrixMultiply( Q, h, h, A, h, w, temp);	
+
+MatrixTranspose(Q, h, h, Qt);
+
+MatrixMatrixMultiply(Q, h, h, Qt, h, h , temp1);
+printf("hopefully idenitity\n");
+prettyPrint(temp1, h, h);
+
+MatrixMatrixMultiply( Q, h, h, A, h, w, temp);	
 	
 	printf(" new Q = \n");
 	prettyPrint(Q, h, h);
@@ -115,9 +129,7 @@ for(int k =0; k< n; k++){
 	
 	printf("Q R = \n");
 	prettyPrint(temp, h, w);
-	
 
-}
 
 
 }
@@ -143,11 +155,19 @@ Block( double *outA, const double *inA, int h, int w, int b, int i_bloc1, int i_
 UnBlock( double *outA, const double *inA, int h, int w, int b, int i_bloc1, int i_bloc2, int j_bloc)
 --- Copies elements in block into appropriate 2 spots in matrix
 
-BlockQ(const double *inQ, double *outQ, int hQ, int wQ, int b, int i_bloc1, int j_bloc1, int i_bloc2, int j_bloc2 )
---- Copies appropriate 4 spots in matrix into one block
+BlockQ(const double *inQ, double *outQ, int hQ, int wQ, int b, int j_bloc1, int j_bloc2 )
+--- Copies appropriate 2 sets of columns in matrix into one block
 
-UnBlockQ( const double *inQ, double *outQ, int hQ, int wQ, int b, int i_bloc1, int j_bloc1, int i_bloc2, int j_bloc2)
---- Copies elements in block ito appropriate 4 spots in matrix 
+UnBlockQ( const double *inQ, double *outQ, int hQ, int wQ, int b, int j_bloc1, int j_bloc2)
+--- Copies elements in block into appropriate spots in matrix 
+
+BlockQ1(const double *inQ, double *outQ, int hQ, int wQ, int b, int j_bloc )
+--- Copies appropriate set of columns in matrix into one block
+
+UnBlockQ1( const double *inQ, double *outQ, int hQ, int wQ, int b, int j_bloc)
+--- Copies elements in block into appropriate spots in matrix 
+
+
 */ 
 
 void UnBlock( double *outA, const double *inA, int h, int w, int b, int i_bloc1, int i_bloc2, int j_bloc){
@@ -321,28 +341,104 @@ for(int i=0; i<b; i++){
 }
 
 
+void BlockQ1(const double *inQ, double *outQ, int hQ, int wQ, int b, int j_bloc ){
+/*----------------------------------------------------------------------------- 
+PURPOSE: Takes matrix Q, outputs the desired h by b block pads with zeros when necessary. 
+ARGUEMENTS:
+	hQ: Height of matrix
+	wQ: Width of matrix  
+	b: Blocksize 
+	j_bloc: j Block index
+-----------------------------------------------------------------------------*/
 
-void UnBlockQ( const double *inQ, double *outQ, int hQ, int wQ, int b, int i_bloc1, int j_bloc1, int i_bloc2, int j_bloc2){
+
+/* Calculate the number of rows or columns that need to be padded */
+	int wn_bloc = (wQ+b-1)/b; // Number of Blocks 
+	int wpadding = wn_bloc*b - wQ ; // Number of columns of zeros needed.
+
+
+
+if (j_bloc >= wn_bloc)
+ {
+    fprintf(stderr, "BlockQ1: We need j_bloc2 to be strictly less than the number of j blocks, %d!\n", wn_bloc);
+    abort(); 
+  } 
+
+/* Copy Block - j_bloc */
+        for(int i=0; i<hQ; i++){
+		for(int j=0; j<b; j++){
+			if( (j_bloc == wn_bloc -1) && (j >= (b-wpadding))){
+			outQ[i + j*hQ] = 0;
+			} else{
+			outQ[i + j*hQ] = inQ[ i + (j+ j_bloc*b)*hQ ] ;
+			} 
+		}	 
+	}
+
+
+}
+
+
+void UnBlockQ1( const double *inQ, double  *outQ, int hQ, int wQ, int b, int j_bloc){
+/*----------------------------------------------------------------------------- 
+PURPOSE: Takes block of Q, Replaces the values of A. Gets rid of pads if necessary.  
+ARGUEMENTS: 
+	b: Blocksize
+	hQ: height of Q
+	wQ: width of Q  
+	j_bloc: j Block index
+-----------------------------------------------------------------------------*/
+
+
+/* Calculate the number of rows or columns padded that needs to be removed*/
+	int wn_bloc = (wQ+b-1)/b; // Number of Blocks 
+	int wpadding = wn_bloc*b - wQ ; // Number of columns of zeros needed.
+
+
+/* Check to make sure that j_bloc < wn_bloc */
+
+if (j_bloc >= wn_bloc)
+ {
+    fprintf(stderr, "We need j_bloc2 to be strictly less than the number of j blocks, %d!\n", wn_bloc);
+    abort(); 
+  } 
+
+/* Block - j_bloc */
+
+    if( ( wpadding == 0) || ( j_bloc != wn_bloc -1)){
+	for(int i=0; i<hQ; i++){
+		for(int j=0; j<b; j++){
+		outQ[ i + (j+j_bloc*b)*hQ ] = inQ[i + j*hQ];
+		}
+	} 
+    }else{
+	if( j_bloc == wn_bloc -1){
+	for(int i=0; i<hQ; i++){
+		for(int j=0; j<(b-wpadding); j++){
+		outQ[ i+ + (j+j_bloc*b)*hQ ] = inQ[ i + j*hQ];
+		}
+	}
+	}
+	
+     }
+
+
+
+}
+
+void UnBlockQ( const double *inQ, double *outQ, int hQ, int wQ, int b, int j_bloc1, int j_bloc2){
 /*----------------------------------------------------------------------------- 
 PURPOSE: Takes block of Q, Replaces the values of A. Gets rid of pads if necessary.  
 ARGUEMENTS:
 	n: Size of matrix 
 	b: Blocksize
 	hQ: height of Q
-	wQ: width of Q
-	The next four arguments tell us the two distinct columns and two distinct rows the new Q will effect. 
-	i_bloc1: i Block index, uppermost i block index  
+	wQ: width of Q  
 	j_bloc1: j Block index,  leftmost j block index
-	i_bloc2: i Block index
 	j_bloc2: j Block index 
 -----------------------------------------------------------------------------*/
 
-/* Check to make such i_bloc1 < i_bloc2 and j_bloc1 < j_block2 */
-if (i_bloc1 >= i_bloc2)
-  {
-    fprintf(stderr, "We need i_bloc1 to be strictly greater than i_bloc2!\n");
-    abort(); 
-  } 
+/* Check to make such j_bloc1 < j_block2 */
 
 if (j_bloc1 >= j_bloc2)
  {
@@ -353,17 +449,10 @@ if (j_bloc1 >= j_bloc2)
 
 /* Calculate the number of rows or columns padded that needs to be removed*/
 	int wn_bloc = (wQ+b-1)/b; // Number of Blocks 
-	int hn_bloc = (hQ+b -1)/b; // Number of Blocks
 	int wpadding = wn_bloc*b - wQ ; // Number of columns of zeros needed.
-	int hpadding = hn_bloc*b - hQ ; // Number of rows of zeros needed.
 
 
-/* Check to make sure that i_bloc2 < hn_bloc and j_bloc < wn_bloc */
-if (i_bloc2 >= hn_bloc)
-  {
-    fprintf(stderr, "We need i_bloc2 to be strictly less than the number of i blocks, %d!\n", hn_bloc);
-    abort(); 
-  } 
+/* Check to make sure that j_bloc < wn_bloc */
 
 if (j_bloc2 >= wn_bloc)
  {
@@ -372,158 +461,48 @@ if (j_bloc2 >= wn_bloc)
   } 
 
 
-/* First Block - i_bloc1, j_bloc1 */
-    if( ( wpadding == 0 && hpadding == 0) || (i_bloc1 != hn_bloc-1 && j_bloc1 != wn_bloc -1)){
-	for(int i=0; i<b; i++){
+/* First Block - j_bloc1 */
+	for(int i=0; i<hQ; i++){
 		for(int j=0; j<b; j++){
-		outQ[ i+i_bloc1*b + (j+j_bloc1*b)*hQ ] = inQ[i + 2*j*b];
+		outQ[i + (j+ j_bloc1*b)*hQ] = inQ[i + j*hQ];
+		}
+	}
+
+/* Second Block - j_bloc2 */
+
+    if( ( wpadding == 0) || ( j_bloc2 != wn_bloc -1)){
+	for(int i=0; i<hQ; i++){
+		for(int j=0; j<b; j++){
+		outQ[ i + (j+j_bloc2*b)*hQ ] = inQ[hQ*b + i + j*hQ];
 		}
 	} 
     }else{
-	if( i_bloc1 == hn_bloc -1 && j_bloc1 != wn_bloc -1){
-	for(int i=0; i<(b-hpadding); i++){
-		for(int j=0; j<b; j++){
-		outQ[ i+i_bloc1*b + (j+j_bloc1*b)*hQ ] = inQ[i + 2*j*b];
-		}
-	}
-	}
-	if( j_bloc1 == wn_bloc -1 && i_bloc1 != hn_bloc -1){
-	for(int i=0; i<b; i++){
+	if( j_bloc2 == wn_bloc -1){
+	for(int i=0; i<hQ; i++){
 		for(int j=0; j<(b-wpadding); j++){
-		outQ[ i+i_bloc1*b + (j+j_bloc1*b)*hQ ] = inQ[i + 2*j*b];
+		outQ[ i+ + (j+j_bloc2*b)*hQ ] = inQ[hQ*b + i + j*hQ];
 		}
 	}
 	}
-	if( i_bloc1 == hn_bloc -1 && j_bloc1 == wn_bloc -1){
-	for(int i=0; i<(b-hpadding); i++){
-		for(int j=0; j<(b-wpadding); j++){
-		outQ[ i+i_bloc1*b + (j+j_bloc1*b)*hQ ] = inQ[i + 2*j*b];
-		}
-	}
-    	}
-   }
+	
+     }
 
 
-/* Second Block - i_bloc1, j_bloc2 */
-
-if( ( wpadding == 0 && hpadding == 0) || (i_bloc1 != hn_bloc-1 && j_bloc2 != wn_bloc -1)){
-	for(int i=0; i<b; i++){
-		for(int j=0; j<b; j++){
-		outQ[ i+i_bloc1*b + (j+j_bloc2*b)*hQ ] = inQ[2*b*b + i + j*2*b];
-		}
-	} 
-    }else{
-	if( i_bloc1 == hn_bloc -1 && j_bloc2 != wn_bloc -1){
-	for(int i=0; i<(b-hpadding); i++){
-		for(int j=0; j<b; j++){
-		outQ[ i+i_bloc1*b + (j+j_bloc2*b)*hQ ] = inQ[2*b*b + i + j*2*b];
-		}
-	}
-	}
-	if( j_bloc2 == wn_bloc -1 && i_bloc1 != hn_bloc -1){
-	for(int i=0; i<b; i++){
-		for(int j=0; j<(b-wpadding); j++){
-		outQ[ i+i_bloc1*b + (j+j_bloc2*b)*hQ ] = inQ[2*b*b + i + j*2*b];
-		}
-	}
-	}
-	if( i_bloc1 == hn_bloc -1 && j_bloc2 == wn_bloc -1){
-	for(int i=0; i<(b-hpadding); i++){
-		for(int j=0; j<(b-wpadding); j++){
-		outQ[ i+i_bloc1*b + (j+j_bloc2*b)*hQ ] = inQ[2*b*b + i + j*2*b];
-		}
-	}
-    	}
-   }
-
-
-/* Third Block - i_bloc2, j_bloc1 */
-
-     if( ( wpadding == 0 && hpadding == 0) || (i_bloc2 != hn_bloc-1 && j_bloc1 != wn_bloc -1)){
-	for(int i=0; i<b; i++){
-		for(int j=0; j<b; j++){
-		outQ[ i+i_bloc2*b + (j+j_bloc1*b)*hQ ] = inQ[b+i + j*2*b];
-		}
-	} 
-    }else{
-	if( i_bloc2 == hn_bloc -1 && j_bloc1 != wn_bloc -1){
-	for(int i=0; i<(b-hpadding); i++){
-		for(int j=0; j<b; j++){
-		outQ[ i+i_bloc2*b + (j+j_bloc1*b)*hQ ] = inQ[b+i + j*2*b];
-		}
-	}
-	}
-	if( j_bloc1 == wn_bloc -1 && i_bloc2 != hn_bloc -1){
-	for(int i=0; i<b; i++){
-		for(int j=0; j<(b-wpadding); j++){
-		outQ[ i+i_bloc2*b + (j+j_bloc1*b)*hQ ] = inQ[b+i + j*2*b];
-		}
-	}
-	}
-	if( i_bloc2 == hn_bloc -1 && j_bloc1 == wn_bloc -1){
-	for(int i=0; i<(b-hpadding); i++){
-		for(int j=0; j<(b-wpadding); j++){
-		outQ[ i+i_bloc2*b + (j+j_bloc1*b)*hQ ] = inQ[b+i + j*2*b];
-		}
-	}
-    	}
-   }
-
-
-/* Fourth Block - i_bloc2, j_bloc2 */
-  if( ( wpadding == 0 && hpadding == 0) || (i_bloc2 != hn_bloc-1 && j_bloc2 != wn_bloc -1)){
-	for(int i=0; i<b; i++){
-		for(int j=0; j<b; j++){
-		outQ[ i+i_bloc2*b + (j+j_bloc2*b)*hQ ] = inQ[2*b*b +b +i + 2*j*b];
-		}
-	} 
-    }else{
-	if( i_bloc2 == hn_bloc -1 && j_bloc2 != wn_bloc -1){
-	for(int i=0; i<(b-hpadding); i++){
-		for(int j=0; j<b; j++){
-		outQ[ i+i_bloc2*b + (j+j_bloc2*b)*hQ ] = inQ[2*b*b +b +i + 2*j*b];
-		}
-	}
-	}
-	if( j_bloc2 == wn_bloc -1 && i_bloc2 != hn_bloc -1){
-	for(int i=0; i<b; i++){
-		for(int j=0; j<(b-wpadding); j++){
-		outQ[ i+i_bloc2*b + (j+j_bloc2*b)*hQ ] = inQ[2*b*b +b +i + 2*j*b];
-		}
-	}
-	}
-	if( i_bloc2 == hn_bloc -1 && j_bloc2 == wn_bloc -1){
-	for(int i=0; i<(b-hpadding); i++){
-		for(int j=0; j<(b-wpadding); j++){
-		outQ[ i+i_bloc2*b + (j+j_bloc2*b)*hQ ] = inQ[2*b*b +b +i + 2*j*b];
-		}
-	}
-    	}
-   }
 
 }
 
-void BlockQ(const double *inQ, double *outQ, int hQ, int wQ, int b, int i_bloc1, int j_bloc1, int i_bloc2, int j_bloc2 ){
+void BlockQ(const double *inQ, double *outQ, int hQ, int wQ, int b, int j_bloc1, int j_bloc2 ){
 /*----------------------------------------------------------------------------- 
 PURPOSE: Takes matrix Q, outputs the desired 2b by 2b block, pads with zeros when necessary. 
 ARGUEMENTS:
 	hQ: Height of matrix
 	wQ: Width of matrix  
-	b: Blocksize
-	The next four arguments tell us the two distinct columns and two distinct rows the new Q will effect. 
-	i_bloc1: i Block index, uppermost i block index  
+	b: Blocksize 
 	j_bloc1: j Block index,  leftmost j block index
-	i_bloc2: i Block index
 	j_bloc2: j Block index 
 -----------------------------------------------------------------------------*/
 
-/* Check to make such i_bloc1 < i_bloc2 and j_bloc1 < j_block2 */
-if (i_bloc1 >= i_bloc2)
-  {
-    fprintf(stderr, "We need i_bloc1 to be strictly greater than i_bloc2!\n");
-    abort(); 
-  } 
-
+/* Check to make such j_bloc1 < j_block2 < wn_bloc.*/
 
 if (j_bloc1 >= j_bloc2)
  {
@@ -534,70 +513,34 @@ if (j_bloc1 >= j_bloc2)
 
 /* Calculate the number of rows or columns that need to be padded */
 	int wn_bloc = (wQ+b-1)/b; // Number of Blocks 
-	int hn_bloc = (hQ+b -1)/b; // Number of Blocks
 	int wpadding = wn_bloc*b - wQ ; // Number of columns of zeros needed.
-	int hpadding = hn_bloc*b - hQ ; // Number of rows of zeros needed.
 
-/* Copy first Block - i_bloc1, j_bloc1 */
-for(int i=0; i<b; i++){
+if (j_bloc2 >= wn_bloc)
+ {
+    fprintf(stderr, "We need j_bloc2 to be strictly less than the number of j blocks, %d!\n", wn_bloc);
+    abort(); 
+  } 
+
+	
+/* Copy first Block - j_bloc1 */
+      for(int i=0; i<hQ; i++){
 		for(int j=0; j<b; j++){
-		if( (i_bloc1 == hn_bloc -1) && (i>= (b-hpadding))){
-			outQ[i + j*2*b] = 0;
-		} else{
+			outQ[i + j*hQ] = inQ[ i + (j+ j_bloc1*b)*hQ ] ;
+		}	 
+	}
+
+/* Copy second Block - j_bloc2 */
+        for(int i=0; i<hQ; i++){
+		for(int j=0; j<b; j++){
 			if( (j_bloc2 == wn_bloc -1) && (j >= (b-wpadding))){
-			outQ[i + j*2*b] = 0;
+			outQ[hQ*b + i + j*hQ] = 0;
 			} else{
-			outQ[i + j*2*b] = inQ[ i+i_bloc1*b + (j+ j_bloc1*b)*hQ ] ;
+			outQ[hQ*b + i + j*hQ] = inQ[ i + (j+ j_bloc2*b)*hQ ] ;
 			} 
-		}	
-		} 
-	}
-
-/* Copy second Block - i_bloc1, j_bloc2 */
-for(int i=0; i<b; i++){
-		for(int j=0; j<b; j++){
-		if( (i_bloc1 == hn_bloc -1) && (i>= (b-hpadding))){
-			outQ[2*b*b + i + j*2*b] = 0;
-		} else{
-			if( (j_bloc2 == wn_bloc -1) && (j >= (b-wpadding))){
-			outQ[2*b*b + i + j*2*b] = 0;
-			} else{
-			outQ[2*b*b + i + j*2*b] = inQ[ i+i_bloc1*b + (j+ j_bloc2*b)*hQ ] ;
-			} 
-		}		
-		} 
-	}
-
-/* Copy thrid Block - i_bloc2, j_bloc1 */
-for(int i=0; i<b; i++){
-		for(int j=0; j<b; j++){
-		if( (i_bloc2 == hn_bloc -1) && (i>= (b-hpadding))){
-			outQ[b+i + j*2*b] = 0;
-		} else{
-			if( (j_bloc1 == wn_bloc -1) && (j >= (b-wpadding))){
-			outQ[b+i + j*2*b] = 0;
-			} else{
-			outQ[b+i + j*2*b] = inQ[ i+i_bloc2*b + (j+ j_bloc1*b)*hQ ] ;
-			} 
-		}
-		} 
+		}	 
 	}
 
 
-/* Copy fourth Block - i_bloc2, j_bloc2 */
-for(int i=0; i<b; i++){
-		for(int j=0; j<b; j++){
-		if( (i_bloc2 == hn_bloc -1) && (i>= (b-hpadding))){
-			outQ[2*b*b +b +i + 2*j*b] = 0;
-		} else{
-			if( (j_bloc2 == wn_bloc -1) && (j >= (b-wpadding))){
-			outQ[2*b*b +b +i + 2*j*b] = 0;
-			} else{
-			outQ[2*b*b +b +i + 2*j*b] = inQ[ i+i_bloc2*b + (j+ j_bloc2*b)*hQ ] ;
-			} 
-		}
-		} 
-	}
 }
 
 
