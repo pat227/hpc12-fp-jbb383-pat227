a28e2e4abae1edd2e4b68a1f87a67cc00e440e29
paul <paul@x60.westell.com>
2012-12-14 16:35:26 -0500

    Forgot one file.

--- QR/test.h
+++ QR/test.h
@@ -10,12 +10,13 @@ void testUpperTriangular( double  *, int,int);
 
 /* Code to test Blocked Matrix Matrix Multiply */
 //this is now being used--not for testing--by QR code, should be moved to utilities
-void dgemm_simple( const double *, const int, const int, const double *, const int, const int, double *);
+//void dgemm_simple( const double *, const int, const int, const double *, const int, const int, double *);
 
 void testMatrixMultiply( const double *, const double *, double *, int, int, int, int);
 
 /* Code to test Blocked Matrix Transpose */
-void simple_transpose(const double *, int , int , double *);
+//ditto...move to utilities
+//void simple_transpose(const double *, int , int , double *);
 void testMatrixTranspose(const double *, int , int , const double *);
 //Given Q, R & A, test QR=A
 int IsQRequalToA(const double * const Q, const double * const R, 
