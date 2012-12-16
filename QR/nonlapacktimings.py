d0243de1031282adb3762c870f798bb0954e41a0
Paul Torres <pat227@nyu.edu>
2012-12-16 14:30:42 -0500

    Can I push from Bowery?

--- QR/nonlapacktimings.py
+++ QR/nonlapacktimings.py
@@ -10,53 +10,54 @@ import subprocess
 def main(args):
     if len(args) != 3:
         print "Usage: python nonlapacktimings.py n rank verbosity"
-        print "   n         -> the maximum number of iterations to be used, specified as the exponent of 10^n"
+        print "   n         -> the maximum number of iterations to be used, specified as the exponent of 10^(n+1)"
         print "   rank      -> the upper bound of the square matrix size to decompose as exponent of 2^(r+1)"
         print "   algorithm -> one or more of (a,b,c,d)"
         print " Computes the QR decompositions of random square matrices for a range of iterations and sizes"
         print " using the following optional methods: "
-        print "    a)Householder reflectors       b)WY algo w/o blocking"
-        print "    c)Blocked-QR upon method 'b'   d)Blocked-QR upon WY w/blocking"
+        print "    a)Householder reflectors       b)WY"
+        print "    c)Blocked-QR utilizing WY"
 #        print " NOTE: on a quad-core Q6600 intel cpu, with rank=50, 100,000 iterations take about 74 seconds."
         return
     n = int(args[0])
     r = int(args[1])
     methods = str(args[2])
     print("Timing QR decompositions...")
-    if( n > 7 or n < 0):
+    if( n > 7 or n < 1):
         print "N must be a value of 1 through 7 (corresponding to 10^n); exercise caution in selection."
         return
-    if( r > 9 or r < 0):
+    if( r > 9 or r < 1):
         print "R must be a value of 0 through 9 (corresponding to 2^1 - 2^10)"
         return
     sizes = [2,4,8,16,32,64,128,256,512,1024]
     iterations = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000]
-    if(methods.find("a") > -1):
-        print "Starting iterations up to:", iterations[n], " with sizes up to:", sizes[r]
-        for xr in range(r):
-            rlocal = sizes[xr]
-            for xn in range(n):
-                subprocess.call(["./householder", str(rlocal), str(rlocal), str(iterations[xn]), str(0)] )
+#    if(methods.find("a") > -1):
+#        print "Starting iterations up to:", iterations[n], " with sizes up to:", sizes[r]
+#        for xr in range(r):
+#            rlocal = sizes[xr]
+#            for xn in range(n):
+#                subprocess.call(["./householder", str(rlocal), str(rlocal), str(iterations[xn]), str(0)] )
 
     if(methods.find("b") > -1):
-        print "Starting iterations up to:", iterations[n], " with sizes up to:", sizes[r]
+        print "Starting iterations up to:", iterations[n-1], " with sizes up to:", sizes[r-1]
         for xr in range(r):
             rlocal = sizes[xr]
             for xn in range(n):
+#                print "xn:"; print xn; print "xr:"; print xr; print "rlocal"; print rlocal; print "iters:"; print iterations[xn];
                 subprocess.call(["./wy", str(rlocal), str(rlocal), str(iterations[xn]), str(0)] )
 
     if(methods.find("c") > -1):                
-    for xr in range(r):
-        rlocal = sizes[xr]
-        for xn in range(n):
-            subprocess.call(["./BlockedQR", str(rlocal), str(rlocal), str(iterations[xn]), str(0)] )
-  
-    if(methods.find("d") > -1):
-        print "Starting iterations up to:", iterations[n], " with sizes up to:", sizes[r]
         for xr in range(r):
             rlocal = sizes[xr]
             for xn in range(n):
-                subprocess.call(["./BlockedQR2", str(rlocal), str(rlocal), str(iterations[xn]), str(0)] )
+                subprocess.call(["./BlockedQR", str(rlocal), str(rlocal), str(iterations[xn]), str(0)] )
+  
+#    if(methods.find("d") > -1):
+#        print "Starting iterations up to:", iterations[n], " with sizes up to:", sizes[r]
+#        for xr in range(r):
+#            rlocal = sizes[xr]
+#            for xn in range(n):
+#                subprocess.call(["./BlockedQR2", str(rlocal), str(rlocal), str(iterations[xn]), str(0)] )
 
 if __name__ == "__main__":
     import sys
