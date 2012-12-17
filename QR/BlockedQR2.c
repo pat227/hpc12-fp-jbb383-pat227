feda36ea338f47147613560221e5872b6188580f
paul <paul@x60.westell.com>
2012-12-17 16:32:32 -0500

    FiNALLY finished indexing for blocked version 2 for QR; all (obvious bugs killed). Needs cleaning. Needs to be incorporated into QR2

--- QR/BlockedQR2.c
+++ QR/BlockedQR2.c
@@ -14,7 +14,7 @@ Performs Blocked QR factorization - version 2
 #include "Utilities.h"
 #include "BlockedQR.h"
 #include "BlockedQR2.h"
-
+#define verbose 1
 
 void BlockedQR2( double *A, int h, int w, double *Q){
 
@@ -43,76 +43,86 @@ void BlockedQR2( double *A, int h, int w, double *Q){
 	}	
 
 /* Enter Loop */
-for( int k =0 ; k < 1; k++){
+for( int k =0 ; k < n; k++){
   /* Calculate number of blocks below diagonal */
-  int nbloc = hn_bloc - k ; 
+  int nbloc = hn_bloc;
   /* Set initial c1 and c2 */
-  int c = 2;
+  int c = 1;
   int c1 = 1; 
   int c2 = 2; 
-  int boundary = 0;
+  int boundary = k;
   int iteration = 0;
-  int bigiter = 0;
-  int localhn = hn_bloc - boundary;
-  int oldboundary = boundary;
-  int mostrecentpair = 0;
+  //int bigiter = 0;
+  // int localhn = hn_bloc - boundary - k;
+  //int oldboundary = boundary;
   //handle special case of only 1 remaining block
   while(boundary <= hn_bloc){
-    oldboundary = boundary;
-    printf("\n At top of loop: localhn is %d, oldboundary is %d", localhn, oldboundary);
-    if(localhn < 1){
-      printf("STOP HERE: k = %d, nbloc = %d, iteration = %d, Blocks called : %d, %d \n", k, nbloc, iteration, 0, mostrecentpair);
-      break;
-    }
-    if(localhn < 2){
-      printf("STOP HERE: k = %d, nbloc = %d, iteration = %d, Blocks called : %d, %d \n", k, nbloc, iteration, 0, hn_bloc);
-      break;
-    }
+    //oldboundary = boundary;
+    //if(verbose) printf("\n At top of loop: localhn is %d, oldboundary is %d", localhn, oldboundary);
+    
+    //if(localhn < 1){
+      //printf("\nSTOP HERE A: k = %d, nbloc = %d, iteration = %d, Blocks called : %d, %d \n", k, nbloc, iteration, k, oldboundary);
+    // break;
+    //}
+    
+    //if(localhn < 2){
+    //  printf("\nSTOP HERE B: k = %d, nbloc = %d, iteration = %d, Blocks called : %d, %d \n", k, nbloc, iteration, k, hn_bloc-1);
+    //  break;
+    //}
     //compute how many we can evenly fit into binary tree and just do those
-    c = 2;
-    while(c <= localhn){
+    //might want to use math.c log function here & round down, if that would be faster
+    c = 1;
+    while(c <= hn_bloc - boundary + 1){
       c*=2;
     }
-    boundary += c/2;
+    
     c1 = 1;
     c2 = 2;
-    printf(" %d (rd/st/th) boundary at %d \n", bigiter, boundary);
+    printf(" x (nd/rd/st/th) boundary at %d \n", boundary);
     printf(" k = %d, nbloc = %d \n", k, nbloc);		
-    printf("c2 = %d and c: %d  hn_bloc: %d \n", c2, c, hn_bloc);
-    while( c2 <= c/2 ){
+    if(verbose) printf("c2 = %d and c: %d  hn_bloc: %d \n", c2, c, hn_bloc);
+    while( c2 <= c ){
       printf("c2 = %d \n", c2);
       //guard against going over the cliff
-      int loopguard = boundary;
-      if(boundary > hn_bloc) loopguard = hn_bloc;
-      printf("loopguard is : %d\n", loopguard);
+      //int loopguard = boundary;
+      //if(boundary > hn_bloc) loopguard = hn_bloc;
+      //      if(verbose) printf("loopguard is : %d\n", loopguard);
       //only on first iteration do we start at zero, for rest it's prior boundary + 1
-      if(bigiter > 0){
-	printf("Not first iteration...\n");
-	for(int i = oldboundary; i < loopguard; i += c2){
-	  printf("k = %d, nbloc = %d, iteration = %d, Blocks called : %d, %d \n", k, nbloc, iteration, i, i+c1);
-	  mostrecentpair = i;
-	}
-      } else {
-	printf("First iteration...\n");
-	for(int i = k; i < loopguard; i += c2){
-	  printf("k = %d, nbloc = %d, iteration = %d, Blocks called : %d, %d \n", k, nbloc, iteration, i, i+c1);
-	}
+      //if(bigiter > 0){
+      //if(verbose) printf("Not first iteration...\n");
+      for(int i = boundary; i < hn_bloc-c1; i += c2){
+	printf("k = %d, nbloc = %d, iteration = %d, Blocks called : %d, %d \n", k, nbloc, iteration, i, i+c1);
       }
+	//} else {
+	//printf("First iteration...\n");
+	//for(int i = k; i < loopguard; i += c2){
+	//  printf("k = %d, nbloc = %d, iteration = %d, Blocks called : %d, %d \n", k, nbloc, iteration, i, i+c1);
+	//	}
+	//}
       
       /* Update constants c1 and c2 */
       c1 *= 2; 
       c2 *= 2;
       iteration += 1;
     }
-    bigiter++;
-    //now repeat for rest of blocks that are own "trees"
-    iteration = 0;
-    localhn = hn_bloc - boundary;
-    printf("\nBoundary is now (at end of while loop) % d and localhn is %d ", boundary, localhn);
-    //printf("\nlocalhn is %d", localhn);
+    //call root of this binary tree against root of prior binary tree--which is always at zero
+    //    if(bigiter>0){
+    //printf("\nCall root of this most recent binary tree and root of all prior trees--always zero.");
+    //  printf("\nk = %d, nbloc = %d, iteration = %d, Blocks called : %d, %d \n", k, nbloc, iteration, 0, oldboundary);
+  boundary += c;
+  }
+  //bigiter++;
+  //now repeat for rest of blocks that are own "trees"
+  iteration = 0;
+  //localhn = hn_bloc - boundary - k;
+  //if(verbose) printf("\nBoundary is now (at end of while loop) % d and localhn is %d ", boundary);
+  //printf("\nlocalhn is %d", localhn);
+  if(k == hn_bloc - 1 ){
+    printf("k = %d, nbloc = %d, Blocks called : %d \n", k, nbloc, k);    
   }
  }
 }
+
   /*
   while(c < localn){
     c*=2;
